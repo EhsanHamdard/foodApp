971cfa8c7e9f0eecf859b926f59277341cf4ebe4
EhsanHamdard <ehsan.hamdard723@gmail.com>
2021-03-25 11:11:41 -0700

    new updates on Home page

--- Screens/Home.js
+++ Screens/Home.js
@@ -2,11 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ImageSlider from 'react-native-image-slider'
+
 export default function App() {
+	let images = [
+		require("../assets/bg.jpg"),
+		require("../assets/rice.jpg"),
+		require("../assets/steak.jpg"),
+
+	]
   return (
     <View style={styles.container}>
-      <Text>Testing the first one</Text>
-      <StatusBar style="auto" />
+      <View styles.containerImageSlider>
+      	<ImageSlider
+      		style={styles.ImageSlider}
+      		images={images}
+      		autoplayWithInterval={7000}
+      	/>
+
+      </View>
+      <Text style={styles.categoryText}>Categories</Text>
     </View>
   );
 }
@@ -15,5 +30,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+
+  containerImageSlider:{ 
+  height: 170
+  margin: 8 
+},
+ImageSlider:{
+	borderRadius: 10
+},
+
+categoryText:{
+	margin:10,
+	fontSize:16,
+	fontWeight: "bold"
+}
+
 });
 
